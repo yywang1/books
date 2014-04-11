f5ccca301c3af54c9c103e2a7c20fb8722348b8a
yywang1 <yywang1@i9i8.com>
2014-04-11 14:17:03 +0800

    over

--- master/pending.php
+++ master/pending.php
@@ -1,10 +1,12 @@
 <?
-include_once __DIR__ . '../../includes/user.func.php';
+include_once __DIR__ . '../../includes/global.init.php';
 
-if(! checkLogin()) {
-	redirect($WEB_ROOT . "login.php?back=" . $_SERVER['PHP_SELF']);
+if(! isLogin()) {
+	redirect($container['WEB_ROOT'] . "login.php?back=" . $_SERVER['PHP_SELF']);
 }
 
-echo Zandy_Template::outString('master/pending.html', $siteConf['tplDir'], $siteConf['cacheDir']);
+$tplArray['html_main'] = $container['twig']->render('master/pending.html', array());
+
+echo $container['twig']->render('master/c_master_tpl.html', $tplArray);
 
 ?>
